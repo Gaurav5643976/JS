037db7cdfbbdc04130d9a62a94c0110a75f385d5
Gaurav5643976 <mr.gaurav5643976@gmail.com>
2021-01-14 11:36:01 +0530

    14/1/21

---
+++ 02Basics/01Variables.js
@@ -0,0 +1,15 @@
+var fulName="Gaurav";
+
+var courseName="Full stack development";
+
+var isLoggedIn=false;
+
+var loggedCount=34;
+console.log(loggedCount);
+console.log("loggedCount");
+
+var paymentMode;
+console.log(paymentMode);
+paymentMode="Credit Card";
+console.log(paymentMode);
+
---
+++ 02Basics/02Variables.js
@@ -0,0 +1,20 @@
+const uniqueId = "gaurav5643976"; // value cannot be changed so it is constant
+var fullName = "Gaurav Singh";
+var email = "mr.gaurav5643976@gmail.com";
+var password = "snetulshi";
+var confirmPasswor = "snetulshi";
+var courseCount = 0;
+var isLoggedInFromGoogle = false;
+
+//fullName = prompt("Enter your name");
+
+console.log(uniqueId);
+console.log("Full Name is:" + fullName);
+console.log("My Email is:" + email);
+
+
+console.log(`
+    With Unique ID: ${uniqueId}
+    User is: ${fullName}
+    and his email is: ${email}
+`);
